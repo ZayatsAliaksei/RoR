b55fd616678064a79c1370a8d0771426e568ad4c
ZayatsAliaksei <legoza@narod.ru>
2021-03-26 14:29:03 +0300

    4 issue

--- Урок№2/triangle_area.rb
+++ Урок№2/triangle_area.rb
@@ -1,6 +1,6 @@
 puts "Введите основание треугольника:"
-Base = gets.to_f
+base = gets.to_f
 puts "Введите высоту треугольника:"
-Height = gets.to_f
-Area = (Base * Height) / 2
-puts "Площадь: #{Area}"
+height = gets.to_f
+area = (base * height) / 2
+puts "Площадь: #{area}"
